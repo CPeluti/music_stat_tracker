68c2296d6ebb7b8da9c9d1a3a0b2cb0d09b8887a
Caio Peluti <cpeluti@gmail.com>
2025-10-16 19:12:09 -0300

    added sequelize to the project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,8 @@
 import { middleware } from "#middlewares/middleware.js";
 import express from "express";
+import { Sequelize } from "sequelize";
+
+export const sequelize = new Sequelize("sqlite::memory:");
 
 export const app = express();
 const port = process.env.PORT ?? "8080";
